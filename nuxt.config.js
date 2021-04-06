e3a9550a395a492fd5a3aa07f45ced06fc21ba98
Paweł Klimkowski <pawel@sqrtx.pl>
2021-04-06 14:45:31 +0200

    feat: basic configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,15 @@
+import { resolve } from 'path'
+
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+
+  alias: {
+    '@components': resolve(__dirname, './pages/@components')
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,28 @@
-<template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        charactr-ui-challenge
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+  section.root
+    selection(v-model="source")
 </template>
 
 <script>
-export default {}
+
+import CharactrInput from '@components/select-multiple-tags.vue';
+
+export default {
+  components: {
+    'selection': CharactrInput
+  },
+  data() {
+
+    let source = []; // Fetch some slots data
+
+    return {
+      source
+    }
+  }
+}
 </script>
 
-<style>
+<style lang="scss">
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -39,35 +30,5 @@ export default {}
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
