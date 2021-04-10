a03aef101af326de00cb6795f61eb65686f3f981
Piotr Kołpaczyński <kolpaczynskipiotr@gmail.com>
2021-04-10 11:40:18 +0200

    chore: setup project

--- pages/@components/select-multiple-tags.vue
+++ pages/@components/select-multiple-tags.vue
@@ -3,9 +3,22 @@
     .label Hello, I'm not finished UI element, and I need some love.
 </template>
 <script>
+import { onMounted } from '@nuxtjs/composition-api';
+
 export default {
-  
-}
+  props: {
+    value: {
+      type: Array,
+    },
+  },
+  setup(props) {
+
+    onMounted(() => {});
+
+    return {};
+  }
+};
+
 </script>
 <style lang="scss" scoped>
 .charactr-select-input-wrapper {
